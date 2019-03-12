e6988a5672d2110d98a6d8e07d02d69ada24ea32
FelipeBaum <felipe.baumgartel@gmail.com>
2019-03-12 12:47:17 -0300

    att

--- CalculaPI.java
+++ CalculaPI.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package Ex1;
-
-
 /**
  *
- * @author felip
+ * @author felipr
  */
 public class CalculaPI {
 
